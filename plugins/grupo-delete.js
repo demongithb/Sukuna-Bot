67af92933bda8a1d1eff9671d526033fccca2d65
Jxxlzncom <jxxlznexce@gmail.com>
2025-06-21 10:03:39 -0500

    Actualizar grupo-delete.js

--- plugins/grupo-delete.js
+++ plugins/grupo-delete.js
@@ -1,17 +1,17 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `${emoji} Por favor, cita el mensaje que deseas eliminar.`, m)
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
 return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
- } catch {
+} catch {
 return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}
-}
+}}
+
 handler.help = ['delete']
-handler.tags = ['group']
-handler.command = /^del(ete)?$/i
+handler.tags = ['grupo']
+handler.command = ['del','delete']
 handler.group = false
 handler.admin = true
 handler.botAdmin = true
